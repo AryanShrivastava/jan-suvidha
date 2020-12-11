64690b3bc610ca1b4a837779c18ba75fc13babe1
Ankit Hans <ankithans1947@gmail.com>
2020-12-11 19:27:01 +0530

    fix(mobile): upload image

--- mobile/README.md
+++ mobile/README.md
@@ -1,6 +1,6 @@
-# mobile
+# jansuvidha
 
-A new Flutter project.
+A new Flutter application.
 
 ## Getting Started
 
--- mobile/android/app/src/main/kotlin/com/myproject/jansuvidha/MainActivity.kt
+++ mobile/android/app/src/main/kotlin/com/myproject/jansuvidha/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.mobile
+package com.myproject.jansuvidha
 
 import io.flutter.embedding.android.FlutterActivity
 
