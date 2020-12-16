92bf6dd3834fd88df83052b77846119a77c2769e
Dev Aryan Shrivastava <aryanshrivastava36@gmail.com>
2020-12-16 15:47:49 +0530

    Update Mlverify.dart
    ML code update

--- mobile/lib/Mlverify.dart
+++ mobile/lib/Mlverify.dart
@@ -16,7 +16,173 @@ class _Ml extends State<Ml> {
   bool _loading = false;
 
   @override
+  void initState() {import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:jansuvidha/sign_in.dart';
+import 'package:tflite/tflite.dart';
+
+import 'drawer.dart';
+import 'login_page.dart';
+
+//import 'package:google_fonts/google_fonts.dart';
+import 'package:jansuvidha/scratchcard.dart';
+class Ml extends StatefulWidget {
+  @override
+  _Ml createState() => _Ml();
+}
+
+class _Ml extends State<Ml> {
+  List _outputs;
+  File _image;
+  bool _loading = false;
+
+  @override
   void initState() {
+    super.initState();
+    _loading = true;
+
+    loadModel().then((value) {
+      setState(() {
+        _loading = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFFB2FF59),
+          title: RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+                text: 'JAN',
+                style: GoogleFonts.portLligatSans(
+                  textStyle: Theme.of(context).textTheme.display1,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.black,
+                ),
+                children: [
+                  TextSpan(
+                    text: 'SUVIDHA',
+                    style: TextStyle(color: Colors.white, fontSize: 30),
+                  ),
+                ]),
+          ),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text('Logout',
+                  style: new TextStyle(fontSize: 17.0, color: Colors.black)),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => LoginPage()));
+              },
+            ),
+            //Icon(Icons.power_settings_new)
+          ],
+        ),
+        backgroundColor: Colors.white,
+        drawer: Drawer_File(),
+        body: _loading
+            ? Container(
+          alignment: Alignment.center,
+          child: CircularProgressIndicator(),
+        )
+            : Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _image == null ? Container() : Image.file(_image),
+              SizedBox(
+                height: 20,
+              ),
+              _outputs != null
+                  ?
+              "${_outputs[0]["label"]}" != "invalid image" ?
+              RaisedButton(
+                  onPressed: () {
+                    sendImg(_image);
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => ScratchCard()));
+                  },
+                  child: Text(
+                    "${_outputs[0]["label"]}",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20.0,
+                      background: Paint()..color = Colors.white,
+                    ),
+                  ))
+                  :  Text(
+                "${_outputs[0]["label"]}",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20.0,
+                  background: Paint()..color = Colors.white,
+                ),
+              )
+                  : Container(),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: pickImage,
+          child: Icon(Icons.image),
+        ),
+      ),
+    );
+  }
+
+  pickImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    if (image == null) return null;
+    setState(() {
+      _loading = true;
+      _image = image;
+    });
+    classifyImage(image);
+  }
+
+  classifyImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 2,
+      threshold: 0.5,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
+
+    setState(() {
+      _loading = false;
+      _outputs = output;
+    });
+
+
+  }
+
+
+  loadModel() async {
+    await Tflite.loadModel(
+      model: "assets/model_unquant.tflite",
+      labels: "assets/labels.txt",
+    );
+  }
+
+  @override
+  void dispose() {
+    Tflite.close();
+    super.dispose();
+  }
+}
+
     super.initState();
     _loading = true;
 
