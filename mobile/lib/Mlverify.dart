c5db8ce870b826554a91458de5e8ae88d86e10cb
Dev Aryan Shrivastava <aryanshrivastava36@gmail.com>
2020-12-21 17:54:39 +0530

    Update Mlverify.dart

--- mobile/lib/Mlverify.dart
+++ mobile/lib/Mlverify.dart
@@ -33,16 +33,16 @@ class _Ml extends State<Ml> {
   @override
   int currentPage = 0;
   Widget imageCarousel = new Container(
-      height: 220.0,
+      height: 240.0,
       child: new Carousel(
         boxFit: BoxFit.cover,
         images: [
           NetworkImage(
-              'https://thumbs.dreamstime.com/z/cartoon-people-fixing-hole-highway-industrial-city-roadwork-banner-repair-equipment-heavy-machinery-working-pothole-169603809.jpg'),
+              'https://st3.depositphotos.com/1005979/18633/i/600/depositphotos_186339762-stock-photo-pothole-tire-wheel-damage-road.jpg'),
           NetworkImage(
-              'https://thumbs.dreamstime.com/z/car-highway-falling-asphalt-hole-cartoon-man-looking-shock-automobile-pothole-accident-cityscape-backdrop-flat-193690324.jpg'),
+              'https://image.shutterstock.com/image-vector/car-wheel-fell-into-pit-260nw-1491691268.jpg'),
           NetworkImage(
-              'https://media.istockphoto.com/vectors/gift-card-with-blue-bow-vector-id1179439557?k=6&m=1179439557&s=612x612&w=0&h=Gj7OWfomEmRDIZZ_DbA15QImyDaW7-G0DW0AA89G6HY='),
+              'https://i.pinimg.com/originals/60/b3/7d/60b37daaedeaf89b6c886cc294ff1634.jpg'),
         ],
         showIndicator: false,
         animationCurve: Curves.fastOutSlowIn,
@@ -54,18 +54,45 @@ class _Ml extends State<Ml> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(children: <Widget>[
+        SizedBox(height: 10),
         imageCarousel,
         SizedBox(height: 10),
-        RaisedButton(
-            onPressed: pickImage,
-            child: Text(
-              "ML model",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20.0,
-                background: Paint()..color = Colors.white,
+        Container(
+          decoration: new BoxDecoration(
+            border: Border.all(color: Colors.black, width: 4),
+            color: Colors.purple,
+            gradient: new LinearGradient(
+              colors: [Colors.blue, Colors.redAccent],
+            ),
+            boxShadow: [
+              new BoxShadow(
+                color: Color(0xff514a9d),
+                offset: new Offset(20.0, 30.0),
+                blurRadius: 40.0,
+              )
+            ],
+          ),
+          height: 250,
+          width: 350,
+          child: new InkWell(
+            onTap: pickImage,
+            child: Card(
+              elevation: 7.0,
+              child: Container(
+                padding: EdgeInsets.only(
+                    left: 12.0, top: 180.0, right: 2.0, bottom: 5.0),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("images/MLpot.jpg"),
+                    fit: BoxFit.cover,
+                    alignment: Alignment.topCenter,
+                  ),
+                ),
+                // child: Card1(),
               ),
-            )),
+            ),
+          ),
+        ),
         _loading
           ? Container(
         alignment: Alignment.center,
